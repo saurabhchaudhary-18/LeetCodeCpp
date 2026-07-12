2da304888b4f8bcd99a38a80321502610d3fb2f9
Saurabh Singh <saurabh1234singh123@gmail.com>
2026-07-12 11:16:05 +0530

    Time: 6 ms (43.42%), Space: 43.1 MB (99.98%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] arr = new int[26];
+        if(s.length()!=t.length()){
+            return false;
+        }
+        for(int i=0; i<s.length(); i++){
+            arr[s.charAt(i)-'a'] = arr[s.charAt(i)-'a']+1;
+        }
+        for(int i=0; i<t.length(); i++){
+            arr[t.charAt(i)-'a'] = arr[t.charAt(i)-'a']-1;
+        }
+        for(int i=0; i<26; i++){
+            if(arr[i]!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
