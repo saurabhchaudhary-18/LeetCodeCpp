5a7ed4ec09880fa5f3ee6650d27b19b87e3548f8
Saurabh Singh <saurabh1234singh123@gmail.com>
2026-08-08 13:34:15 +0530

    Time: 1 ms (65.27%), Space: 42.4 MB (65.97%) - LeetHub

---
+++ 3178-find-the-child-who-has-the-ball-after-k-seconds/3178-find-the-child-who-has-the-ball-after-k-seconds.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int numberOfChild(int n, int k) {
+        n--;
+        int r = k/n;
+        int rem = k%n;
+        if(r%2==0){
+            return rem;
+        } else {
+            return n-rem;
+        }
+    }
+}
