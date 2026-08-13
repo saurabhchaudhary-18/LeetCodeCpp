618a8e559f47c1193cb9cf912078f2e64ee845c5
Saurabh Singh <saurabh1234singh123@gmail.com>
2026-08-13 14:44:05 +0530

    Time: 4 ms (1.63%), Space: 42.8 MB (8.55%) - LeetHub

---
+++ 0461-hamming-distance/0461-hamming-distance.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int hammingDistance(int x, int y) {
+        //return Integer.bitCount(x^y);
+        int a = x^y;
+        String s = "";
+        while(a!=0){
+            s=a%2+s;
+            a=a/2;
+        }
+        int cnt = 0;
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i)=='1'){
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+}
